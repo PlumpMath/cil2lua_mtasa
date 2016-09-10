f0a6411322b48888dd4feba5689a1b4d83a07aaf
DEC05EBA <aleksi_888@hotmail.com>
2016-09-10 23:29:16 +0200

    Modified license mistake

--- MTASAServer/Console.cs
+++ MTASAServer/Console.cs
@@ -9,7 +9,7 @@ but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
 GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
-along with cil2lua.If not, see <http://www.gnu.org/licenses/>.
+along with cil2lua_mtasa.If not, see <http://www.gnu.org/licenses/>.
 */
 
 namespace MTASAServer
