42867c0aa973c68ab9c53d58e50787d90b38ad81
DEC05EBA <aleksi_888@hotmail.com>
2016-09-10 17:06:02 +0200

    Added all files

---
+++ MTASAShared/Fps.cs
@@ -0,0 +1,22 @@
+﻿/*
+This file is part of cil2lua_mtasa (https://github.com/DEC05EBA/cil2lua_mtasa).
+cil2lua_mtasa is free software : you can redistribute it and / or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+cil2lua_mtasa is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with cil2lua.If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace MTASAShared
+{
+    public static class Fps
+    {
+        static int getLimit() { return 0; } // getFPSLimit()
+        static bool setLimit(int fpsLimit) { return true; } // setFPSLimit(fpsLimit)
+    }
+}
