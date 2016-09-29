6a1c13fcdb27eea717d7cafc2f8ced1cea0fb610
DEC05EBA <aleksi_888@hotmail.com>
2016-09-29 20:00:09 +0200

    Changed license to GPLv2

--- MTASAClient/Graphics.cs
+++ MTASAClient/Graphics.cs
@@ -2,7 +2,7 @@
 This file is part of cil2lua_mtasa (https://github.com/DEC05EBA/cil2lua_mtasa).
 cil2lua_mtasa is free software : you can redistribute it and / or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
+the Free Software Foundation, either version 2 of the License, or
 (at your option) any later version.
 cil2lua_mtasa is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
